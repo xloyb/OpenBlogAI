41cef7cb059fba601c3e34323821f7f9b7064456
mydevify <boussaadilouay9@gmail.com>
2025-01-04 14:54:34 +0100

    created routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './output.css'
-import App from './App.tsx'
+//import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import NotFound from './ui/components/404.tsx'
+import Layout from './ui/components/Layout.tsx'
+import Dashboard from './ui/dashboard/dashboard.tsx'
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} /> 
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+</StrictMode>,
 )
