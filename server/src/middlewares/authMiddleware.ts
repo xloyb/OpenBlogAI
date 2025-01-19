0a34a79d1b287e583e37bde0c3751bef3c616abd
Louay Bousaadi <44674105+xloyb@users.noreply.github.com>
2025-01-19 18:19:29 +0100

    created Authentication Middleware

---
+++ server/src/middlewares/authMiddleware.ts
@@ -0,0 +1,33 @@
+import prisma from "@src/utils/client";
+import { verifyToken } from "@utils/jwt";
+import { Request, Response, NextFunction } from "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
+  }
+}
+
+export const authenticateJWT = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+  try {
+    const tokenRecord = await prisma.refreshToken.findUnique({
+      where: { token },
+    });
+
+    if (!tokenRecord || tokenRecord.isRevoked || tokenRecord.expiresAt < new Date()) {
+      return res.status(403).json({ error: "Invalid or revoked token" });
+    }
+
+    const user = verifyToken(token);
+    req.user = user;
+    next();
+  } catch {
+    res.status(403).json({ error: "Invalid token" });
+  }
+};
