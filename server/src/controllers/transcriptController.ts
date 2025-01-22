548e8631bed4cf884bc2442e63893a61390963b0
Louay Bousaadi <44674105+xloyb@users.noreply.github.com>
2025-01-22 18:29:44 +0100

    created transcript controller

---
+++ server/src/controllers/transcriptController.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express";
+import { YoutubeTranscript } from 'youtube-transcript';
+
+export const extractTranscript = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    // const { videoId } = req.params;
+
+    let videoId = "lVdgiuzGmdI";
+
+    if (!videoId) {
+      res.status(400).json({ error: true, message: "Video ID is required" });
+      return;
+    }
+
+    // Fetch the transcript using the youtube-transcript package
+    const transcript = await YoutubeTranscript.fetchTranscript(videoId);
+
+    // Format the transcript into a single string
+    const formattedTranscript = transcript.map(entry => entry.text).join(' ');
+    console.log("formattedTranscript:",formattedTranscript);
+
+    res.status(200).json({
+      message: "Transcript extracted successfully",
+      transcript: formattedTranscript,
+    });
+  } catch (error) {
+    next(error); 
+  }
+};
