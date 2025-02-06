f762ab5023845327591680913eeb40334b0b1a50
Louay Bousaadi <44674105+xloyb@users.noreply.github.com>
2025-02-06 12:06:52 +0100

    created blog schema

---
+++ server/src/utils/blogSchema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+const SubsectionSchema = z.object({
+  heading: z.string(),
+  content: z.string()
+});
+
+export const BlogSchema = z.object({
+  title: z.string().min(10).max(255),
+  introduction: z.string().min(100),
+  subsections: z.array(SubsectionSchema).min(3),
+  conclusion: z.string().min(100),
+  key_takeaways: z.array(z.string().min(20)).min(3),
+  status: z.enum(['DRAFT', 'PUBLISHED', 'ARCHIVED']).optional(),
+  videoId: z.number().positive()
+});
