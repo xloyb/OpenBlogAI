cc2439f8f1b4cf4abc6cc8294a0038a306b618af
Louay Bousaadi <44674105+xloyb@users.noreply.github.com>
2025-02-06 12:06:16 +0100

    updated blog service

--- server/src/services/blogService.ts
+++ server/src/services/blogService.ts
@@ -1,41 +1,41 @@
-import prisma from "@src/utils/client";
+// import prisma from "@src/utils/client";
 
-// Create a new blog
-export const createBlog = async (videoId: number, title: string, content: string, keywords: string[]) => {
-  return prisma.blog.create({
-    data: {
-      videoId,
-      title,
-      content,
-      keywords: {
-        connectOrCreate: keywords.map((word) => ({
-          where: { word },
-          create: { word },
-        })),
-      },
-    },
-    include: { keywords: true },
-  });
-};
+// // Create a new blog
+// export const createBlog = async (videoId: number, title: string, content: string, keywords: string[]) => {
+//   return prisma.blog.create({
+//     data: {
+//       videoId,
+//       title,
+//       content,
+//       keywords: {
+//         connectOrCreate: keywords.map((word) => ({
+//           where: { word },
+//           create: { word },
+//         })),
+//       },
+//     },
+//     include: { keywords: true },
+//   });
+// };
 
-// Fetch a blog by ID
-export const getBlogById = async (id: number) => {
-  return prisma.blog.findUnique({
-    where: { id },
-    include: { keywords: true },
-  });
-};
+// // Fetch a blog by ID
+// export const getBlogById = async (id: number) => {
+//   return prisma.blog.findUnique({
+//     where: { id },
+//     include: { keywords: true },
+//   });
+// };
 
-// Fetch all blogs
-export const getAllBlogs = async () => {
-  return prisma.blog.findMany({
-    include: { keywords: true },
-  });
-};
+// // Fetch all blogs
+// export const getAllBlogs = async () => {
+//   return prisma.blog.findMany({
+//     include: { keywords: true },
+//   });
+// };
 
-// Delete a blog
-export const deleteBlog = async (id: number) => {
-  return prisma.blog.delete({
-    where: { id },
-  });
-};
+// // Delete a blog
+// export const deleteBlog = async (id: number) => {
+//   return prisma.blog.delete({
+//     where: { id },
+//   });
+// };
