90e595c11284d613884699346de59f558572e4d7
Louay Bousaadi <44674105+xloyb@users.noreply.github.com>
2025-02-07 21:56:43 +0100

    created getSingleBlog route

--- server/src/routes/blogRoutes.ts
+++ server/src/routes/blogRoutes.ts
@@ -1,12 +1,14 @@
 // src/routes/blogRoutes.ts
 import { Router } from "express";
-import { generateBlog, getAllBlogs } from "@controllers/blogController";
+import { generateBlog, getAllBlogs, getSingleBlog } from "@controllers/blogController";
 
 const router = Router();
 
 // Route to generate a blog from a transcript
 router.post("/generate-blog", generateBlog as any);
 router.get('/blogs', getAllBlogs);
+router.get('/blogs/:id', getSingleBlog);
+
 
 
 export default router;
