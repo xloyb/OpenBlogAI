c4f954883fe36803429b860d0892df727d359052
Louay Bousaadi <44674105+xloyb@users.noreply.github.com>
2025-02-07 20:51:26 +0100

    added getAllBlogs route

--- server/src/routes/blogRoutes.ts
+++ server/src/routes/blogRoutes.ts
@@ -1,10 +1,12 @@
 // src/routes/blogRoutes.ts
 import { Router } from "express";
-import { generateBlog } from "@controllers/blogController";
+import { generateBlog, getAllBlogs } from "@controllers/blogController";
 
 const router = Router();
 
 // Route to generate a blog from a transcript
 router.post("/generate-blog", generateBlog as any);
+router.get('/blogs', getAllBlogs);
+
 
 export default router;
