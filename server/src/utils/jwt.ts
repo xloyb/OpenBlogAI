35ef481820d3f73730d9babd4d0cbaf32cb4c8e8
Louay Bousaadi <44674105+xloyb@users.noreply.github.com>
2025-02-16 02:50:57 +0100

    refactor token verification to omit password from user payload

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,3 +1,4 @@
+import { User } from '@prisma/client';
 import { env } from 'envValidator';
 import jwt from 'jsonwebtoken';
 
@@ -5,6 +6,9 @@ export const signToken = (payload: object, expiresIn = '1h') => {
   return jwt.sign(payload, env.JWT_SECRET, { expiresIn });
 };
 
-export const verifyToken = (token: string) => {
-  return jwt.verify(token, env.JWT_SECRET);
+// export const verifyToken = (token: string) => {
+//   return jwt.verify(token, env.JWT_SECRET);
+// };
+export const verifyToken = (token: string): Omit<User, "password"> => {
+  return jwt.verify(token, env.JWT_SECRET) as Omit<User, "password">;
 };
