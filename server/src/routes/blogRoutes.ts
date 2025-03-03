17e6d5ea6ef6715e10468b8e31825568191fbfa0
Louay Bousaadi <44674105+xloyb@users.noreply.github.com>
2025-03-03 21:20:19 +0100

    added JWT authentication to getAllBlogs route

--- server/src/routes/blogRoutes.ts
+++ server/src/routes/blogRoutes.ts
@@ -1,12 +1,13 @@
 // src/routes/blogRoutes.ts
 import { Router } from "express";
 import { deleteExistingBlog, generateBlog, getAllBlogs, getSingleBlog, updateExistingBlog } from "@controllers/blogController";
+import { authenticateJWT, authorizeAdmin } from "@src/middlewares/authMiddleware";
 
 const router = Router();
 
 // Route to generate a blog from a transcript
 router.post("/generate-blog", generateBlog as any);
-router.get('/blogs', getAllBlogs);
+router.get('/blogs', authenticateJWT ,getAllBlogs);
 router.get('/blogs/:id', getSingleBlog);
 router.patch('/blogs/:id', updateExistingBlog);
 router.delete('/blogs/:id', deleteExistingBlog);
