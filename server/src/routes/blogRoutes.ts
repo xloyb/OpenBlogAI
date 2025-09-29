f2520af872bee14cb80a495bf73e98591daa7a8a
Louay Bousaadi <44674105+xloyb@users.noreply.github.com>
2025-09-29 21:41:27 +0100

    Add route to fetch available models in blogRoutes

--- server/src/routes/blogRoutes.ts
+++ server/src/routes/blogRoutes.ts
@@ -1,13 +1,15 @@
 // src/routes/blogRoutes.ts
 import { Router } from "express";
-import { deleteExistingBlog, generateBlog, getAllBlogs, getSingleBlog, updateExistingBlog } from "@controllers/blogController";
+import { deleteExistingBlog, generateBlog, getAllBlogs, getSingleBlog, updateExistingBlog, getAvailableModels } from "@controllers/blogController";
 import { authenticateJWT, authorizeAdmin } from "@src/middlewares/authMiddleware";
 
 const router = Router();
 
+// Route to get available models
+router.get('/models', getAvailableModels);
 // Route to generate a blog from a transcript
 router.post("/generate-blog", generateBlog as any);
-router.get('/blogs', authenticateJWT ,getAllBlogs);
+router.get('/blogs', authenticateJWT, getAllBlogs);
 router.get('/blogs/:id', getSingleBlog);
 router.patch('/blogs/:id', updateExistingBlog);
 router.delete('/blogs/:id', deleteExistingBlog);
